f12f7d8bc5637cf8c0880ea6a45d8d4d680c2875
Christopher Baum <christopherabaum@gmail.com>
2022-11-09 18:55:40 -0600

    Add 3 arrays

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var colors = ["blue", "red", "orange", "pink"]
+var luckyNums = [13, 37, 23, 11, 21]
+
+var hungry = false
+var tired = true
+var sad = false
+var grumpy = false
+
+var feelings = [hungry, tired, sad, grumpy]

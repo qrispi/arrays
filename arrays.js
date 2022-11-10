541b5cfea340414c9623ef8feabfafc6e6e365ed
Christopher Baum <christopherabaum@gmail.com>
2022-11-09 19:12:28 -0600

    Add 3 array methods

--- arrays.js
+++ arrays.js
@@ -1,9 +1,24 @@
 var colors = ["blue", "red", "orange", "pink"]
-var luckyNums = [13, 37, 23, 11, 21]
+var luckyNums = [13, 37, 23, 11]
+var boolean = [(2 === 3), (5 > 7), (3 === 3), (4 !== 5)]
 
-var hungry = false
-var tired = true
-var sad = false
-var grumpy = false
+console.log(colors.pop())
+console.log(colors)
 
-var feelings = [hungry, tired, sad, grumpy]
+// This method will remove the last element from the array and return the value of that element.
+// Expected output of the method will be pink.
+// Expected output of the array will now be [ 'blue', 'red', 'orange' ].
+
+console.log(luckyNums.push(21))
+console.log(luckyNums)
+
+// This method will add a new element, in this case 21, to the end of the array and return the new length of the array as an integer.
+// Expected output of the method will be 5.
+// Expected output of the array will now be [ 13, 37, 23, 11, 21 ].
+
+console.log(boolean.shift())
+console.log(boolean)
+
+// This method will remove the first element, in this case (2 === 3), from the array and return the value of that element.
+// Expected output of the method will be false since 2 != 3.
+// Expected output of the array will now be [ false, true, true ].
